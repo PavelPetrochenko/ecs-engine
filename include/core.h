4e9ab65396561b60096293c3566f0cd7383e9cf8
deadbit-dev <pavelpetrochenko2001@gmail.com>
2021-10-01 17:10:24 +0300

    config CMake

---
+++ include/core.h
@@ -0,0 +1,6 @@
+#ifndef CORE_H
+#define CORE_H
+
+#define VERSION 1.0
+
+#endif
---
+++ src/core.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../include/core.h"
+
+int main ()
+{
+    printf("VERSION %.1f", VERSION);
+    return 0;
+}
