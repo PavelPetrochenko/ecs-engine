ec13b4a1fbb10a04cd12819d4503e269f42ed3ef
Pavel <pavelpetrochenko2001@gmail.com>
2021-10-09 03:09:53 +0300

    remove meson add CMake configurations

--- build.sh
+++ build.sh
@@ -11,23 +11,22 @@ GREEN='\033[1;92m'
 RED='\033[1;31m'
 NC='\033[0m'
 
-echo -e \\a${GREEN}[MESON BUILD]${NC}
-if (exec meson setup build)
+echo -e \\a${GREEN}[GENERATION]${NC}
+if ()
 then
-    echo -e \\a${GREEN}[NINJA BUILD]${NC}
-    if (exec meson compile -C build)
+	echo -e \\a${GREEN}[BUILD]${NC}
+    if ()
     then
-	echo -e \\a${GREEN}[TEST]${NC}
-	if (exec meson test -C build -v)
-	then
-	    echo -e \\a${GREEN}[ALL COMPLETE]${NC}
-	else
-	    echo -e \\a${RED}[TEST ERROR]${NC}
-	fi
+		echo -e \\a${GREEN}[TEST]${NC}	
+		if ()
+		then
+			echo -e \\a${GREEN}[ALL COMPLETE]${NC}    
+		else
+			echo -e \\a${RED}[TEST ERROR]${NC}	    
+		fi
     else
-	echo -e \\a${RED}[NINJA BUILD ERROR]${NC}
+		echo -e \\a${RED}[BUILD ERROR]${NC}	
     fi
 else
-    echo -e \\a${RED}[MESON BUILD ERROR]${NC}
+	echo -e \\a${RED}[GENERATION ERROR]${NC}
 fi
-
