60931a7351c8da95eb85f9af6be1e4455ee9d4e7
Pavel <pavelpetrochenko2001@gmail.com>
2021-10-11 21:11:38 +0300

    update cmake and actions

--- tests/main_test.cpp
+++
@@ -1,9 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-#include "core.h"
-
-TEST_CASE("testing the print_version function"){
-    // CHECK(ECSENGINE_VERSION_MAJOR == 1);
-    // CHECK(ECSENGINE_VERSION_MINOR == 0);
-    CHECK(1);
-}
