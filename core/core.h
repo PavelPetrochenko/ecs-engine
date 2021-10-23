c0b4020eddeafb5f67409465ac464e3a876ea073
Pavel <pavelpetrochenko2001@gmail.com>
2021-10-23 21:21:10 +0300

    cmake conf

--- core/core.h
+++
@@ -1,9 +0,0 @@
-#ifndef CORE_H
-#define CORE_H
-
-#include <stdio.h>
-#include "utils.h"
-
-void print_version();
-
-#endif
---
+++ source/core/utils/log.cpp
@@ -0,0 +1,6 @@
+#include <utils/log.h>
+
+void core::utils::log::print_version()
+{
+    printf("VERSION %d.0%d", 0, 1); // FIXME: change format
+}
