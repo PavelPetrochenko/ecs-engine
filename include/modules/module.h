ab80935fe37352057b18672088daefdf89a64f99
Pavel <pavelpetrochenko2001@gmail.com>
2021-10-29 02:17:53 +0300

    fix add include

---
+++ include/modules/module.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <utils/utils.h>
+
+namespace modules::module
+{
+    ENGINE_API ENGINE_INLINE int r_one();
+}
---
+++ include/utils/log.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <utils/utils.h>
+#include <stdio.h>
+
+namespace core::utils::log
+{
+    ENGINE_API ENGINE_INLINE void print_version();
+}
